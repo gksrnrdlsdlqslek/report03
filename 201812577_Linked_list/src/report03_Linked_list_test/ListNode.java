0eca207e9e307fedde608a25d8f7248610bb738f
user <user@DESKTOP-GB7TIGA>
2020-04-06 01:36:28 +0900

    LienKed List Test Singly Linked List

---
+++ 201812577_Linked_list/src/report03_Linked_list_test/ListNode.java
@@ -0,0 +1,24 @@
+package report03_Linked_list_test;
+
+public class ListNode {
+	private String data;
+	public ListNode link;
+	public ListNode() {
+		this.data=null;
+		this.link=null;
+	}
+
+	public ListNode(String data) {
+		this.data= data;
+		this.link= null;
+	}
+
+	public ListNode(String data,ListNode link) {
+		this.data= data;
+		this.link= link;
+	}
+
+	public String getDate() {
+		return this.data;
+	}
+}
